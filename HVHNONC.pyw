902406c1ad03dcd59c8cdfdfbe5b60fca2d9ee3a
alchenerd <alchenerd@gmail.com>
2018-12-04 20:26:05 +0800

    Make Login module work

--- HVHNONC.pyw
+++ HVHNONC.pyw
@@ -7,28 +7,27 @@ Created on Tue Oct  2 11:08:53 2018
 @project name: Hualien Veterans Home NON-Consumables dbms (HVHNONC)
 """
 
-#import tkinter as tk
 from PyQt5 import QtWidgets
 
 import init
-#import gui
 from myqtpy.Index import Index
+from myqtpy.Login import Login
+
+def login():
+    dialog = QtWidgets.QDialog()
+    Login(dialog)
+    return dialog.exec_()
+
 
 def main():
-    """
-    init.buildDatabase()
-    root = tk.Tk()
-    gui.Index(root)
-    root.mainloop()
-    root.quit()
-    """
-    init.buildDatabase()
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = QtWidgets.QMainWindow()
-    Index(mainWindow)
-    mainWindow.show()
-    sys.exit(app.exec_())
+    if login():
+        init.buildDatabase()
+        mainWindow = QtWidgets.QMainWindow()
+        Index(mainWindow)
+        mainWindow.show()
+        sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
--- myqtpy/Login.py
+++ myqtpy/Login.py
@@ -1,24 +1,43 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  4 13:54:57 2018
-
 @author: alchenerd (alchenerd@gmail.com)
 """
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from myqtpy._login_skeleton import Ui_Form as Form
-
-class Login(QtWidgets.QWidget, Form):
-    def __init__(self, parent):
-        super(self.__class__, self).__init__(parent)
-        self.setupUi(parent)
-        self.show()
+from PyQt5 import QtWidgets
+if __name__ == '__main__':
+    from _login_skeleton import Ui_Dialog as LoginDialog
+else:
+    from myqtpy._login_skeleton import Ui_Dialog as LoginDialog
 
 
-if __name__ == "__main__":
+class Login(QtWidgets.QDialog, LoginDialog):
+    def __init__(self, dialog):
+        super(self.__class__, self).__init__(dialog)
+        self.setupUi(dialog)
+
+    def setupUi(self, Dialog):
+        super(self.__class__, self).setupUi(Dialog)
+        self.submitBtn.clicked.connect(lambda: self.on_submit(Dialog))
+
+    def on_submit(self, Dialog):
+        if self.authenticate():
+            Dialog.accept()
+
+    def authenticate(self):
+        # TODO: Needs a more serious authentication
+        if (self.username.text() == 'administrator'
+                and self.password.text() == 'hualienveteranshome'):
+            return True
+        else:
+            QtWidgets.QMessageBox.critical(self, u'錯誤', u'帳號或密碼不正確!')
+            return False
+
+
+if __name__ == '__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    mainWindow = QtWidgets.QMainWindow()
-    index = Login(mainWindow)
-    mainWindow.show()
+    Dialog = QtWidgets.QDialog()
+    ui = Login()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
